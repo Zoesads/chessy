e31e6ce4449d6950931efb0cd65459442c3bbc99
Zoesads <83200085+Zoesads@users.noreply.github.com>
2022-05-26 23:17:14 +0700

    Added colors

---
+++ includes/colorize.h
@@ -0,0 +1,26 @@
+#ifndef _COLORIZE_
+#define _COLORIZE_
+
+#define clr_style_rset "\033[0m"
+#define clr_style_bold "\033[1m"
+
+#define clr_fg_black "\033[30m"
+#define clr_fg_red "\033[31m"
+#define clr_fg_green "\033[32m"
+#define clr_fg_orange "\033[33m"
+#define clr_fg_blue "\033[34m"
+#define clr_fg_cyan "\033[36m"
+#define clr_fg_white "\033[37m"
+#define clr_fg_lightred "\033[91m"
+#define clr_fg_yellow "\033[93m"
+#define clr_fg_lightcyan "\033[96m"
+
+#define clr_bg_black "\033[40m"
+#define clr_bg_red "\033[41m"
+#define clr_bg_orange "\033[43m"
+#define clr_bg_blue "\033[44m"
+#define clr_bg_purple "\033[45m"
+#define clr_bg_cyan "\033[46m"
+#define clr_bg_white "\033[47m"
+
+#endif
--- src/printer.c
+++ src/printer.c
@@ -7,6 +7,7 @@ void chessy_welcome() {
 
 void chessy_about() {
     printf("About Chessy:\n");
+    printf(" > A terminal chess\n");
     printf(" > Author: Zoesads\n");
     printf(" > Written in C\n");
     printf(" > Source code: https://github.com/Zoesads/chessy\n");
